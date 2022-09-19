39b907c8295d6229fcaee96fea2c211c1e12b0b3
Yovany Cunha <yovany33@gmail.com>
2022-09-18 21:42:41 -0300

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,29 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withImages = require('next-images');
+const withFonts = require('nextjs-fonts');
 
-module.exports = nextConfig
+const path = require('path');
+
+module.exports = withImages(
+  withFonts({
+    sassOptions: {
+      cssModules: true,
+      includePaths: [path.join(__dirname, 'src')],
+    },
+    pageExtensions: ['page.tsx'],
+    handleImages: ['jpg', 'jpeg', 'png', 'webp'],
+    images: {
+      domains: ['images.unsplash.com'],
+    },
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+
+      return config;
+    },
+    env: {
+      STAGE: process.env.STAGE,
+    },
+  })
+);
